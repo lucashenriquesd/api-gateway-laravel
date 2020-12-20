04f2ccbffb7eea7badf9bff0a98887c859fbddb2
Lucas <lucashenriquesd@gmail.com>
2020-12-20 06:37:10 -0300

    Added MS_FILES_LUMEN in .env.example

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Http;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,22 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+
+Route::get('/', function (Request $request) {
+    return response()->json([
+        'msg' => 'Hello World',
+    ]);
+});
+
+Route::get('/ms-files-lumen', function (Request $request) {
+    $response = Http::withHeaders([
+        'app-token' => env('MS_FILES_LUMEN'),
+    ])->get('http://ms-files-lumen8-nginx/');
+
+    return response()->json([
+        'msg' => $response->body(),
+    ]);
+});
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
